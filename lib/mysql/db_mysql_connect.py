8c87a2e9c115ff3432b2c68f4a990b3bbc2efe70
Giacomo Sanchietti <giacomo.sanchietti@nethesis.it>
2020-02-12 17:58:31 +0100

    Remove mysql support

--- lib/mysql/db_mysql_connect.py
+++
@@ -1,9 +0,0 @@
-import pymysql
-
-def mysql_connection():
-    mydb = mysql.connector.connect(
-        host="localhost",
-        user="yourusername",
-        passwd="yourpassword",
-        database="mydatabase"
-    )
